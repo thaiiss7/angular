0106bcde5ef4bee7565507461f2c6c45f5ac4ef5
thaiiss7 <thaismichel7@gmail.com>
2026-03-19 11:25:16 -0300

    add features and shared folders

--- roteamento/src/app/app-routing.module.ts
+++ roteamento/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainPageComponent } from './features/main-page/main-page.component';
+import { LoginPageComponent } from './features/login-page/login-page.component';
+import { NotFoundPageComponent } from './features/not-found-page/not-found-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: MainPageComponent},
+  {path: "login", component: LoginPageComponent},
+  {path: "404", component: NotFoundPageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- roteamento/src/app/app.module.ts
+++ roteamento/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './features/login-page/login-page.component';
+import { MainPageComponent } from './features/main-page/main-page.component';
+import { NotFoundPageComponent } from './features/not-found-page/not-found-page.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { NavComponent } from './shared/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    MainPageComponent,
+    NotFoundPageComponent,
+    HeaderComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
